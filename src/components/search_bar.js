74995683d86620e226731aec714badc2319c9634
eukk <eukk@fnsvalue.co.kr>
2021-07-29 17:56:43 +0900

    commit2

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,7 +4,7 @@ class SearchBar extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { term: '' };
+    this.state = { term: "" };
   }
 
   render() {
@@ -12,14 +12,15 @@ class SearchBar extends Component {
       <div className="search-bar">
         <input
           value={this.state.term}
-          onChange={event => this.onInputChange(event.target.value)}/>
+          onChange={(event) => this.onInputChange(event.target.value)}
+        />
       </div>
     );
   }
 
   onInputChange(term) {
-      this.setState({term});
-      this.props.onSearchTermChange(term);
+    this.setState({ term });
+    this.props.onSearchTermChange(term);
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,44 +1,46 @@
-import _ from 'lodash';
-import React, {Component}from "react";
+import _ from "lodash";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import YTSearch from 'youtube-api-search';
+import YTSearch from "youtube-api-search";
 import SearchBar from "./components/search_bar";
 import VideoList from "./components/video_list";
 import VideoDetail from "./components/video_detail";
 const API_KEY = "AIzaSyAc5I1lzVSij9yUjZy6WnhJMrDIADbZlLw";
 
-
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.state = {
-      videos:[],
-      selectedVideo: null
+      videos: [],
+      selectedVideo: null,
     };
-    this.videoSearch('surfboards');
+    this.videoSearch("surfboards");
   }
 
-  videoSearch(term){
-    YTSearch({key : API_KEY, term: term}, (videos) => {
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term }, (videos) => {
       this.setState({
         videos: videos,
-        selectedVideo: videos[0]
+        selectedVideo: videos[0],
       });
     });
   }
 
   render() {
-    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 100);
+    const videoSearch = _.debounce((term) => {
+      this.videoSearch(term);
+    }, 300);
 
     return (
-    <div>
-      <SearchBar onSearchTermChange={videoSearch}/>
-      <VideoDetail video={this.state.selectedVideo} />
-      <VideoList 
-        onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
-        videos={this.state.videos} />
-    </div>
+      <div>
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={(selectedVideo) => this.setState({ selectedVideo })}
+          videos={this.state.videos}
+        />
+      </div>
     );
   }
 }
