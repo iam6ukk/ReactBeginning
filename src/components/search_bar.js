d9d5ecfe8c341bf3e0b73270b395e5e0a43aa44b
eukk <eukk@fnsvalue.co.kr>
2021-07-30 17:58:35 +0900

    adafafa

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -18,7 +18,7 @@ class SearchBar extends Component {
       <div className="search-bar">
         <input
           value={this.state.term}
-          // this.state의 term값 가져와 인풋값 랜더링
+          // this.state의 term값 가져와 value
           onChange={(event) => this.onInputChange(event.target.value)}
           // onChange발생 시, onInputChange가 term과 함께 호출
         />
